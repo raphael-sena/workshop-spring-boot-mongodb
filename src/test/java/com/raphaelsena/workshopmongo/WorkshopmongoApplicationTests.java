2b0b275fbc31a11cbf32ab1194cdde3a15cb95e7
Raphael Sena <rsenares1@gmail.com>
2023-12-11 10:48:54 -0300

    First commit - project created

---
+++ src/test/java/com/raphaelsena/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphaelsena.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
